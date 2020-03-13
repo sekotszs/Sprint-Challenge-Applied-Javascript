9aa020ae9280ef3a291327044f406cf4531262be
Zoe Stokes <sekotszs@gmail.com>
2020-03-13 09:43:24 -0700

    done

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,54 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+//AXIOS
+const addCards = document.querySelector('div.cards-container');
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+.then(response => {
+    Object.values(response.data.articles).forEach(article => {
+        article.forEach(oneCard =>{
+            addCards.appendChild(theCards(oneCard))
+        })
+    })
+})
+.catch(error => {
+    console.log("the data was not returned",error)
+})
+
+
+function theCards(data) {
+//CARD
+const card = document.createElement('div');
+card.classList.add('card');
+
+//HEADLINE
+const headline = document.createElement('div');
+headline.classList.add('headline');
+headline.textContent = data.headline;
+card.appendChild(headline);
+
+//AUTHOR
+const author = document.createElement('div');
+author.classList.add('author');
+card.appendChild(author);
+
+//IMGDIV
+const imgDiv = document.createElement('div')
+imgDiv.classList.add('img-container')
+author.appendChild(imgDiv);
+
+//IMG
+const img = document.createElement('img');
+img.setAttribute('src', data.authorPhoto)
+imgDiv.appendChild(img);
+
+//AUTHORS NAME
+const aName = document.createElement('span');
+aName.textContent = `By ${data.authorName}`;
+card.appendChild(aName);
+author.appendChild(aName);
+
+return card;
+
+}
